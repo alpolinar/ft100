cbe962d24f676d074b20a41be472607ce409233d
Al Polinar <alpolinar@gmail.com>
2025-04-20 13:28:48 -0600

    add next config

--- packages/frontend/next.config.ts
+++ packages/frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "standalone",
+    reactStrictMode: true,
+    productionBrowserSourceMaps: false,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
 };
 
 export default nextConfig;
